232bf9a38149b46e867f5d2fc4b68afc5b322376
Zac Chan <sunshinecltzac@gmail.com>
2016-04-26 00:29:39 +0800

    增加中间SaySomething及Talkings部分，修改footer

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   title = 'PKU Connector';
-   username = "Pikka Cho";
-   groupList = [{"title": "软双", "href" : "/"},
-                 {"title": "运动会", "href" : "/"},
-                 {"title": "database", "href" : "/"}];
-   talkingList = [];
-   data = {title: title, username: username, groupList: groupList, talkingList: talkingList}
+   var title = 'PKU Connector';
+   var username = "Pikka Cho";
+   var groupList = [{title: "软双", href: "/"},
+                    {title: "运动会", href: "/"},
+                    {title: "database", href: "/"}];
+   var talkingList = [{username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
+                      {username: "寇雨婷", text: "233333333333", timestamp: "2016/4/20 16:05", thumbImg: "/images/demo_thumb_image.jpeg"}];
+   var data = {title: title, username: username, groupList: groupList, talkingList: talkingList}
    res.render('Index', {args: JSON.stringify(data)})
 });
 
