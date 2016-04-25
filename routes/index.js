739213ccb89185ccb37b6e6d96b7fd58e5dd58a3
Zac Chan <sunshinecltzac@gmail.com>
2016-04-25 22:11:57 +0800

    把静态数据移到路由

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   res.render('Index', {args: JSON.stringify({ title: 'PKU Connector', username: 'PikkaCho'})})
+   title = 'PKU Connector';
+   username = "Pikka Cho";
+   groupList = [{"title": "软双", "href" : "/"},
+                 {"title": "运动会", "href" : "/"},
+                 {"title": "database", "href" : "/"}];
+   talkingList = [];
+   data = {title: title, username: username, groupList: groupList, talkingList: talkingList}
+   res.render('Index', {args: JSON.stringify(data)})
 });
 
 module.exports = router;
