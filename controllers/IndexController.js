a625ef2bd7eaedde531976dcd66f71defbadc4a0
Zac Chan <sunshinecltzac@gmail.com>
2016-04-26 18:21:56 +0800

    把逻辑移入Controller

---
+++ controllers/IndexController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ChenLetian on 16/4/26.
+ */
+
+exports.indexPage = function (req, res, next) {
+    var title = 'PKU Connector';
+    var username = "Pikka Cho";
+    var groupList = [{title: "软双", href: "/"},
+        {title: "运动会", href: "/"},
+        {title: "database", href: "/"}];
+    var talkingList = [{username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
+        {username: "寇雨婷", text: "233333333333", timestamp: "2016/4/20 16:05", thumbImg: "/images/demo_thumb_image.jpeg"}];
+    var data = {title: title, username: username, groupList: groupList, talkingList: talkingList}
+    res.render('Index', {args: JSON.stringify(data)})
+}
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-   var title = 'PKU Connector';
-   var username = "Pikka Cho";
-   var groupList = [{title: "软双", href: "/"},
-                    {title: "运动会", href: "/"},
-                    {title: "database", href: "/"}];
-   var talkingList = [{username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
-                      {username: "寇雨婷", text: "233333333333", timestamp: "2016/4/20 16:05", thumbImg: "/images/demo_thumb_image.jpeg"}];
-   var data = {title: title, username: username, groupList: groupList, talkingList: talkingList}
-   res.render('Index', {args: JSON.stringify(data)})
-});
+var indexController = require("../controllers/IndexController");
+router.get('/', indexController.indexPage);
 
 module.exports = router;
