5022634fb9780948bd4fae87f63b409041d6a42f
Zac Chan <sunshinecltzac@gmail.com>
2016-04-26 19:27:00 +0800

    让Index内容多了一些

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -9,7 +9,12 @@ exports.indexPage = function (req, res, next) {
         {title: "运动会", href: "/"},
         {title: "database", href: "/"}];
     var talkingList = [{username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
-        {username: "寇雨婷", text: "233333333333", timestamp: "2016/4/20 16:05", thumbImg: "/images/demo_thumb_image.jpeg"}];
+        {username: "寇雨婷", text: "233333333333", timestamp: "2016/4/20 16:05", thumbImg: "/images/demo_thumb_image.jpeg"},
+        {username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
+        {username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
+        {username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
+        {username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"},
+        {username: "胡顺昕", text: "啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈", timestamp: "2016/4/20 16:10", thumbImg: "/images/demo_thumb_image.jpeg"}];
     var data = {title: title, username: username, groupList: groupList, talkingList: talkingList}
     res.render('Index', {args: JSON.stringify(data)})
 }
