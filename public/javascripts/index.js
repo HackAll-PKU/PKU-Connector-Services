ded8bf28546a85db2da0038f9487c106f66ec9dd
S.Hu <ihsx@qq.com>
2016-04-28 20:33:58 +0800

    搜索框：已有内容后不再显示searching

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,15 @@
 /**
  * Created by ChenLetian on 16/4/28.
  */
-$(document).ready(function() {
-    $("input#search_input").focus(function() {
-        $("span.placeholder").text("");
+$(document).ready(function () {
+    $("input#search_input").focus(function () {
+        //$("span.placeholder").text("");
+        $("span.placeholder")[0].style.visibility = "hidden";
     });
-    $("input#search_input").blur(function() {
-        $(this).val("");
-        $("span.placeholder").text("searching");
+    $("input#search_input").blur(function () {
+        //$(this).val("");
+        if ($(this)[0].value == "")
+            $("span.placeholder")[0].style.visibility = "visible";
+            //$("span.placeholder").text("searching");
     });
 })
