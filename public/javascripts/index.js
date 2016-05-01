e821eb9aa66a5b337677b6664513a74d89696bfa
Zac Chan <sunshinecltzac@gmail.com>
2016-05-01 18:26:10 +0800

    tweaks

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,11 +2,12 @@
  * Created by ChenLetian on 16/4/28.
  */
 $(document).ready(function () {
-    $("input#search_input").focus(function () {
-        $("span.placeholder").hide();
-    });
-    $("input#search_input").blur(function () {
-        if ($(this)[0].value == "")
-            $("span.placeholder").show();
-    });
-})
+    $("input#search_input")
+        .focus(function () {
+            $("span.placeholder").hide();
+        })
+        .blur(function () {
+            if ($(this)[0].value == "")
+                $("span.placeholder").show();
+        });
+});
