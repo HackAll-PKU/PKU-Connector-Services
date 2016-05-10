065469fc21c829dc99164f69925620a0ce4912be
YutingKou <yutingkou@pku.edu.cn>
2016-05-10 20:35:29 +0800

    搭建model里的talking框架

--- models/Talking.js
+++ models/Talking.js
@@ -1,7 +1,47 @@
 /**
  * Created by Kou Yuting on 2016/5/9.
  */
-var mysql = require('mysql');
-var pool  = mysql.createPool(require("../conf/db.json"));
+var pool = require("./BaseModel.js");
+var defaultConf = require("../conf/default.json");
 
-module.exports = pool;
+/**
+ * 说说构造方法
+ * @param tid tid
+ * @param text 说说文本
+ * @param image 说说图片
+ * @param uid 发布者的uid
+ * @param gid talking所在group
+ * @param timestamp 发布说说的时间信息
+ */
+function Talking(tid, text, image, uid, gid, timestamp) {
+    this.tid = tid;
+    this.text = text;
+    this.image = image;
+    this.uid = uid;
+    this.gid = gid;
+    this.timestamp = timestamp;
+}
+
+/**
+ * 添加新说说
+ *  @param completionHandler 返回闭包,包含err, result
+ */
+Talking.prototype.addTalkingToDatabase = function (completionHandler) {
+
+}
+/**
+ * 获取uid的说说
+ *  @param completionHandler 返回闭包,包含err和rows
+ */
+Talking.prototype.getMyTalkings = function (completionHandler) {
+    
+}
+/**
+ * 删除该条说说
+ * @param completionHandler 返回闭包,包含err和affectedRows
+ */
+Talking.prototype.deleteTalking = function(completionHandler){
+
+}
+
+exports.Talking = Talking;
