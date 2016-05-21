a95dfafe1d91984b3a84d85ccbdc1e4008b935b7
Zac Chan <sunshinecltzac@gmail.com>
2016-05-21 10:58:33 +0800

    允许跨域请求

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
 
 // set up router
 var router = require('./routes/router.js');
