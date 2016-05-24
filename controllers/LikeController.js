1a80686ad161a704403c8aff5ab25c6775e04c17
YutingKou <yutingkou@pku.edu.cn>
2016-05-24 20:42:09 +0800

    赞的controller写好了

--- controllers/LikeController.js
+++ controllers/LikeController.js
@@ -1,3 +1,41 @@
 /**
- * Created by lenovo on 2016/5/24.
+ * Created by kouyuting on 2016/5/24.
  */
+var model = require("../models/Like.js");
+/**
+ * 给talking点赞
+ */
+exports.likeUser = function (req,res) {
+    var likeRelation = new model.LikeRelation(req.tokenInfo.uid, req.params.tid);
+    likeRelation.likeUser(function (err) {
+        if(err)
+            res.status(err.code).json({msg:err.msg});
+        else
+            res.status(201).json({msg:"OK"});
+    })
+}
+/**
+ * 取赞说说
+ */
+exports.unlikeUser = function (req,res) {
+    var likeRelation = new model.likeRelation(req.tokenInfo.uid, req.params.tid);
+    likeRelation.unlikeUser(function(err){
+        if (err)
+            res.status(err.code).json({msg:err.msg});
+        else
+            res.json({msg:"OK"});
+    })
+}
+
+/**
+ * 获取当前说说赞数
+ */
+exports.getLikeCount = function(req,res){
+    var likeRelation = new model.likeRelation(req.tokenInfo.uid, req.params.tid);
+    likeRelation.getLikeCount(function (err) {
+        if (err)
+            res.status(err.code).json({msg: err.msg});
+        else
+            res.json({msg: "OK", data: result});        
+    })
+}
