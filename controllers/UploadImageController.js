e2e927938436c63a49d4667496d77e7bec2678c0
Zac Chan <sunshinecltzac@gmail.com>
2016-06-25 12:22:55 +0800

    修改图片上传的路径为绝对URL

--- controllers/UploadImageController.js
+++ controllers/UploadImageController.js
@@ -10,7 +10,7 @@ exports.uploadImage = function (req, res) {
 
     if (req.files) {
         var filenames = req.files.map(function (oneImage) {
-            return "/uploads/" + oneImage.filename
+            return "http://pikkacho.cn/uploads/" + oneImage.filename
         });
         res.json({
             msg: "OK",
